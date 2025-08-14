6279dbd439615388013ed4efe7b9592cce155c25
LayGord <TWISTBOY08@gmail.com>
2025-08-14 20:22:10 +0300

    added Navbar and ThemeSwitcher widgets, svg\file loaders and some ui components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,17 +7,17 @@ import { MainPage } from "pages/MainPage";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
 
-    const { theme, toggleTheme } = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}> О сайте</Link>
+            <Navbar />
             <AppRouter />
+
         </div>
     )
 };
