a67408fd8037f6c41a8222aecb270aaed57404c0
LayGord <TWISTBOY08@gmail.com>
2025-12-11 03:10:52 +0300

    fix CI screenshot testing (again)

---
+++ loki.ci.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configurations: {
+        'chrome-ci.laptop': {
+            target: 'chrome.app',
+            width: 1366,
+            height: 768,
+        },
+        'chrome-ci.iphone7': {
+            target: 'chrome.app',
+            preset: 'iPhone 7',
+        },
+    },
+};
