60cb3959ca9bd5af91c9a7bb7653bb86d2f4b8fd
Gord Lay <TWISTBOY08@gmail.com>
2026-03-02 19:44:54 +0300

    rework color palettes for Light and Dark themes, add Green theme

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -16,7 +16,25 @@ export function useTheme(): UseThemeResult {
     applyTheme(theme || Theme.LIGHT);
 
     const toggleTheme = () => {
-        const newTheme = theme == Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+        //const newTheme = theme == Theme.LIGHT ? Theme.DARK : Theme.LIGHT; // old way
+
+        let newTheme = Theme.LIGHT; // new way
+
+        switch (theme) {
+        case Theme.LIGHT:
+            newTheme = Theme.DARK;
+            break;
+        case Theme.DARK:
+            newTheme = Theme.GREEN;
+            break;
+        case Theme.GREEN:
+            newTheme = Theme.LIGHT;
+            break;
+        default:
+            newTheme = Theme.LIGHT
+            break;
+        }
+
         applyTheme(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
         setTheme?.(newTheme);
