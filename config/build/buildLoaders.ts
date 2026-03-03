dd7a13db62fffa0e28e7e2e5c77e8cd5f7c63a64
Gord Lay <TWISTBOY08@gmail.com>
2026-03-03 21:28:29 +0300

    add concurrently, folder templates and react-refresh-webpack-plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,22 +1,13 @@
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 import { buildScssLoader } from './loaders/buildScssLoader';
 import { buildSvgLoader } from './loaders/buildSvgLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
 export function buildLoaders(buildOptions: BuildOptions): webpack.RuleSetRule[] {
     const { isDev } = buildOptions;
 
-    const babelLoader = {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: "babel-loader",
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    };
+    const babelLoader = buildBabelLoader(buildOptions);
 
     const svgLoader = buildSvgLoader();
 
